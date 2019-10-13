ec2e86aeed0136ff7372627b1a72e2e19e9bc709
jesslam <49288633+jesslam@users.noreply.github.com>
2019-10-13 10:34:52 -0700

    Update for loop and format

--- Assignment3.java
+++ Assignment3.java
@@ -4,25 +4,20 @@ public class Assignment3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-
-		printPowersOfN(7,3);
-		System.out.println();
+		printPowersOfN(7,3); 
+		System.out.println(); 
 		printPowersOfN(5,5);
 		System.out.println();
-
-			
+		 
 		}
-		
-
-	public static void printPowersOfN(int a, int b) {
-		// TODO Auto-generated method stub
-		
-		for (int i = 1; i > 0; i = i * a) {
-			 System.out.print(i + " ");
-		}
-		
-	}
-
+	
+	  public static void printPowersOfN(int a, int b) { 
+		  // TODO Auto-generated method stub
+	  
+		  for (int i = 1; i >= 0; i = i * a) { 
+		  System.out.print(i + " "); 
+		  } 
+	  }
 }
+	
 
