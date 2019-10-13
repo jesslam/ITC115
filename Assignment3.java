3e53e84db797a0d61c2ce0fd10b491cfaa13d1e9
jesslam <49288633+jesslam@users.noreply.github.com>
2019-10-12 22:27:45 -0700

    Add Assignment 3 via upload

---
+++ Assignment3.java
@@ -0,0 +1,28 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+		printPowersOfN(7,3);
+		System.out.println();
+		printPowersOfN(5,5);
+		System.out.println();
+
+			
+		}
+		
+
+	public static void printPowersOfN(int a, int b) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 1; i > 0; i = i * a) {
+			 System.out.print(i + " ");
+		}
+		
+	}
+
+}
+
