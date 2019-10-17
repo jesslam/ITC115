2077af727bbd1518f12245498c8032c9e7133d06
jesslam <49288633+jesslam@users.noreply.github.com>
2019-10-16 18:59:54 -0700

    Upload 10-16-19

---
+++ If10_16_19.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class If10_16_19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		/*
+		 * Scanner console = new Scanner(System.in); System.out.print("Location? ");
+		 * String age = console.nextLine();
+		 * 
+		 * System.out.print("Live with parents? (true or false) "); boolean live =
+		 * console.nextBoolean();
+		 * 
+		 * System.out.print("Salary? "); double salary = console.nextDouble();
+		 * 
+		 * boolean dateTonight = isDate(age, live, salary);
+		 * 
+		 * }
+		 * 
+		 * public static boolean isDate(String location, boolean live, double salary) {
+		 * boolean appLocation = location.equals("Seattle"); boolean appLive = live !=
+		 * true; boolean appSalary = salary >= 100000.00;
+		 * 
+		 * if(appLocation && appLive && appSalary) {
+		 * System.out.println("Okay, let's go out!"); return true; } else {
+		 * System.out.println("Byeee"); return false; }
+		 */
+		
+		/*
+		 * Scanner console = new Scanner(System.in);
+		 * 
+		 * int sum = 0; System.out.println("How many numbers? "); int n =
+		 * console.nextInt();
+		 * 
+		 * for (int i = 1; i <= n; i++) { //sum += i;
+		 * System.out.print("Type a number: "); sum += console.nextInt(); }
+		 * System.out.println("The sum is: " + sum);
+		 */
+		
+		/*
+		 * String food = "pizza"; char firstLetter = food.charAt(2);
+		 * 
+		 * System.out.println(firstLetter + " is part of " + food);
+		 */
+		
+/*		String instrument = "trumpet";
+		for (int i = 0; i < instrument.length(); i++) {
+			char c = instrument.charAt(i);
+			System.out.println(c);*/
+		
+		for (char c = 'A'; c <= 'z'; c++) {
+			System.out.print(c);
+		}
+
+	}
+
+}
