59987607ae1ea2f30f7b206bf38d7b09dab54580
jesslam <49288633+jesslam@users.noreply.github.com>
2019-10-21 16:52:14 -0700

    Add files via upload

---
+++ Assignment4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner console = new Scanner(System.in);
+		//obtain user input for word
+		System.out.println("Type a word: ");
+		String word = console.nextLine();
+		
+		System.out.println("Type a number: ");
+		int number = console.nextInt();
+				
+			}
+	public static String repl(String word) {
+		
+		//obtain user input number of times word will repeat
+		//int number = 0;
+		
+		for (int i = 1; i <= 10; i++) {
+			
+			return (word);
+			//System.out.print(repl);
+			
+		}
+		System.out.print(repl);
+
+
+	}
+
+
+
+
+}
