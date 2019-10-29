5b4b0980d1491447761c3153487469e53b99fc32
jesslam <49288633+jesslam@users.noreply.github.com>
2019-10-28 18:17:55 -0700

    Upload midterm

---
+++ Midterm_playingCard.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+public class Midterm_playingCard {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner console = new Scanner(System.in);
+		System.out.println("Enter a card (rank, suit): ");
+		String rank = console.next();
+		String suit = console.next();
+		
+		playingCards(rank, suit);
+		playingCards("a", "h");
+		
+		console.close();
+
+	}
+	
+	public static void playingCards(String rank, String suit) {
+
+
+		if (rank.equalsIgnoreCase("a")) {
+			System.out.print("Ace");
+		}
+		else if (rank.equalsIgnoreCase("2")) {
+			System.out.print("Two");
+		}
+		else if (rank.equalsIgnoreCase("3")) {
+			System.out.print("Three");
+		}
+		else if (rank.equalsIgnoreCase("4")) {
+			System.out.print("Four");
+		}
+		else if (rank.equalsIgnoreCase("5")) {
+			System.out.print("Five");
+		}
+		else if (rank.equalsIgnoreCase("6")) {
+			System.out.print("Six");
+		}
+		else if (rank.equalsIgnoreCase("7")) {
+			System.out.print("Seven");
+		}
+		else if (rank.equalsIgnoreCase("8")) {
+			System.out.print("Eight");
+		}
+		else if (rank.equalsIgnoreCase("9")) {
+			System.out.print("Nine");
+		}
+		else if (rank.equalsIgnoreCase("10")) {
+			System.out.print("Ten");
+		}
+		else if (rank.equalsIgnoreCase("j")) {
+			System.out.print("Jack");
+		}
+		else if (rank.equalsIgnoreCase("q")) {
+			System.out.print("Queen");
+		}
+		else if (rank.equalsIgnoreCase("k")) {
+			System.out.print("King");
+		}
+		
+		if (suit.equalsIgnoreCase("c")) {
+			System.out.println(" of Clubs");
+		}
+		else if (suit.equalsIgnoreCase("d")) {
+			System.out.println(" of Diamonds");
+		}
+		else if (suit.equalsIgnoreCase("h")) {
+			System.out.println(" of Hearts");
+		}
+		else if (suit.equalsIgnoreCase("s")) {
+			System.out.println(" of Spades");
+		}
+		System.out.println();
+		
+	}
+	
+}
