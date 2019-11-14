d46578c4c45553ce436f1c4e3642ebfe16b91e1b
Jess Lam <49288633+jesslam@users.noreply.github.com>
2019-11-14 10:45:04 -0800

    Add comments and fix code placement

--- Assignment6.java
+++ Assignment6.java
@@ -19,46 +19,44 @@ public class Assignment6 {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		
+
+		// confirm file exists in correct directory
+		//File in = new File("boysgirls.txt");
+		//System.out.println(in.exists());
+
+		// scan file for input
+		Scanner input = new Scanner(new File("boysgirls.txt"));
+		boyGirl(input);
+
 	}
-	
+
 	public static void boyGirl(Scanner input) throws FileNotFoundException {
-		Scanner scan = new Scanner(new File("boysgirls.txt"));
-
-
+		// initialize variables
 		int boySum = 0;
 		int girlSum = 0;
-		int count = 1;
-
-	
-	while (scan.hasNext()) {
-		
-		String list = input.nextLine();
-		System.out.println(list);
-		
 		int boyCount = 0;
 		int girlCount = 0;
-		
-		if (count%2 == 0) {
-			boyCount++;
-			boySum += scan.nextInt();
-		} else {
-			girlCount++;
-			girlSum += scan.nextInt();
-		}
+		int count = 1;
 
+		// loop through each line of list
+		while (input.hasNext()) {
+			String list = input.next();
+			// System.out.print(list);
+			// designate alternating lines group each gender
+			if (count % 2 == 0) {
+				boyCount++;
+				boySum += input.nextInt();
+			} else {
+				girlCount++;
+				girlSum += input.nextInt();
+			}
+			count++;
+
+		}
+		int difference = Math.abs(boySum - girlSum);
+		System.out.println(boyCount + " boys, " + girlCount + " girls");
+		System.out.println("Difference between boys' and girls' sums: " + difference);
 
 	}
-	count++;
-	
-	int difference = Math.abs(boySum - girlSum);
-	System.out.println(boySum + " boys, " + girlSum + " girls");
-	System.out.println("Difference between boys' and girls' sums: " +  difference);
-	
 
 }
-	
-	
-}
-
-
